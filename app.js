152284e65a1d829d128b70b42f133c3389fa703e
Matthew Olivarez <29942482+MatthewOlivarez@users.noreply.github.com>
2021-09-08 10:31:09 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('node_modules'))
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+
+/*app.get('/', (req, res) => {
+	app.use('index')
+  //res.send('Hello World!')
+})*/
+
+app.get('/', function (req, res) {
+	res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
 
 // using fetch
 /*
@@ -26,4 +50,16 @@ axios.get('https://newton.now.sh/api/v2/derive/x%5E2')
 	console.log("ERROR", e)
 })
 */
+/*
+const getResult = () => {
+	try 
+	{
+		const res = axios.get('https://newton.now.sh/api/v2/derive/x%5E2')
+		console.log(res.data.result)
+	} 
+	catch (err)
+	{
+		console.log("ERROR", err)
+	}
+}*/
 
